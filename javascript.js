c06716874ed9ab09edfe7e9e556c7109df620bee
Pablogit config --global user.email psierra.js@gmail.com <pablos@MacBook-Pro-de-Pablo.local>
2022-05-03 11:05:11 +0200

    created function computerChoice returns randomly "rock!", "paper!" and "scissors!" strings

--- javascript.js
+++ javascript.js
@@ -1,3 +1,14 @@
-function computerPlay() {
-  
+/*let computerChoice = 0;*/
+let randomNumber = (Math.floor(Math.random() * 9));
+
+function computerChoice() {
+  if (randomNumber <= 2) {
+    console.log("Rock!");
+  } else if (randomNumber >= 6) {
+    console.log("Paper!");
+  } else {
+    console.log("Scissors!")
+  }
 }
+
+computerChoice();
