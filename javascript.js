dce89347e03883a96fbc9603f6526426d0ea0e8a
Pablogit config --global user.email psierra.js@gmail.com <pablos@MacBook-Pro-de-Pablo.local>
2022-05-03 12:46:22 +0200

    added function playRound() Takes input form player and compares with randomly generated play. Used if else. Can be simplified later.

--- javascript.js
+++ javascript.js
@@ -12,13 +12,44 @@ function computerPlay() {
   let randomNumber = (Math.floor(Math.random() * 3));
   switch (randomNumber) {
     case 0:
-      return "Rock!"
+      return "Rock"
     case 1:
-      return "Paper!"
+      return "Paper"
     case 2:
       return "Scissors"
   }
 }
+const computerSelection = computerPlay().toLowerCase();
+console.log(computerSelection);
 
-const computerDecision = computerPlay();
-console.log(computerDecision);
+/*Defining parameter with player selection. toLowerCase() makes it
+case insensitive*/
+
+let playerSelection = window.prompt("Choose Rock, Paper or Scissors :");
+playerSelection = playerSelection.toLowerCase();
+
+console.log(playerSelection);
+
+
+/*Function playRound below*/
+
+function playRound(playerSelection,computerSelection) {
+  if (computerSelection == playerSelection) {
+    return "There is a tie!";
+  } else if (computerSelection == "rock" && playerSelection == "paper") {
+    return "You Won! Paper! beats rock!"
+  } else if (computerSelection == "rock" && playerSelection == "scissors") {
+    return "You Lost! Rock beats Scissors"
+  } else if (computerSelection == "paper" && playerSelection == "scissors") {
+    return "You won! Scissorts beat paper"
+  } else if (computerScience == "paper" && playerSelection == "rock") {
+    return "You lost! Paper beats rock"
+  } else if (computerSelection == "scissors" && playerSelection == "paper") {
+    return "You lost! Scissors beat paper"
+  } else if (computerSelection == "scissort" && playerSelection == "rock") {
+    return "You won! Rock bears Scissors"
+  }
+
+}
+
+console.log(playRound(playerSelection,computerSelection));
