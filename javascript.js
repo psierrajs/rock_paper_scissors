cf0d4ca62c1f350f86d5244f41ae6b580eb82aeb
Pablogit config --global user.email psierra.js@gmail.com <pablos@MacBook-Pro-de-Pablo.local>
2022-05-03 11:52:29 +0200

    change computerChoice to ComputerPlay Needed to fit the requirements of the assignment

--- javascript.js
+++ javascript.js
@@ -1,14 +1,14 @@
 /*let computerChoice = 0;*/
 
 
-/*Function computerChoice returns randomly "rock!", "paper!" and "scissors!"
+/*Function computerPlay returns randomly "rock!", "paper!" and "scissors!"
 strings.
 Uses a random number between 0 and 3 generated with
 Math.floor() and Math.random() functions.
 According to the value of randomNumber (above) computerChoice returns one
 of the three strings*/
 
-function computerChoice() {
+function computerPlay() {
   let randomNumber = (Math.floor(Math.random() * 3));
   switch (randomNumber) {
     case 0:
@@ -20,5 +20,5 @@ function computerChoice() {
   }
 }
 
-const computerPlay = computerChoice();
-console.log(computerPlay);
+const computerDecision = computerPlay();
+console.log(computerDecision);
