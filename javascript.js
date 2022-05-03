6529a5ad12f7a99aee0e7e101f11c88a720f1fdd
Pablogit config --global user.email psierra.js@gmail.com <pablos@MacBook-Pro-de-Pablo.local>
2022-05-03 11:09:54 +0200

    explained computerChoice function in code

--- javascript.js
+++ javascript.js
@@ -1,6 +1,13 @@
 /*let computerChoice = 0;*/
 let randomNumber = (Math.floor(Math.random() * 9));
 
+/*Function computerChoice returns randomly "rock!", "paper!" and "scissors!"
+strings.
+Uses a random number between 0 and 9 generated with
+Math.floor() and Math.random() functions.
+According to the value of randomNumber (above) computerChoice returns one
+of the three strings*/
+
 function computerChoice() {
   if (randomNumber <= 2) {
     console.log("Rock!");
