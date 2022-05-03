31993abe43e845446910d2d1be20cbad23a4ad24
Pablogit config --global user.email psierra.js@gmail.com <pablos@MacBook-Pro-de-Pablo.local>
2022-05-03 11:48:40 +0200

    simplified computerChoice function with switch

--- javascript.js
+++ javascript.js
@@ -1,21 +1,24 @@
 /*let computerChoice = 0;*/
-let randomNumber = (Math.floor(Math.random() * 9));
+
 
 /*Function computerChoice returns randomly "rock!", "paper!" and "scissors!"
 strings.
-Uses a random number between 0 and 9 generated with
+Uses a random number between 0 and 3 generated with
 Math.floor() and Math.random() functions.
 According to the value of randomNumber (above) computerChoice returns one
 of the three strings*/
 
 function computerChoice() {
-  if (randomNumber <= 2) {
-    console.log("Rock!");
-  } else if (randomNumber >= 6) {
-    console.log("Paper!");
-  } else {
-    console.log("Scissors!")
+  let randomNumber = (Math.floor(Math.random() * 3));
+  switch (randomNumber) {
+    case 0:
+      return "Rock!"
+    case 1:
+      return "Paper!"
+    case 2:
+      return "Scissors"
   }
 }
 
-computerChoice();
+const computerPlay = computerChoice();
+console.log(computerPlay);
