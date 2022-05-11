0f9b1a8e80cc92f0de48e8a0cfbd00bf73f3164e
Pablogit config --global user.email psierra.js@gmail.com <pablos@MacBook-Pro-de-Pablo.local>
2022-05-11 12:51:48 +0200

    Added game() function included and if block and changed console.log to return in order to avoid "undefined" Needs more work specially informing computer and player score.

--- javascript.js
+++ javascript.js
@@ -2,9 +2,9 @@
 //rock, papers or scissors depending on the number's value.
 
 
-let randomNumber = Math.floor(Math.random()*(3)+1);
 
 function computerPlay () {
+  let randomNumber = Math.floor(Math.random()*(3)+1);
   switch (randomNumber) {
     case 1:
       return "rock";
@@ -22,27 +22,52 @@ function computerPlay () {
 //Function playRound(). It takes two parameters (playerSelection and computerSelection)
 //and then returns a string declaring the winner of the round.
 
-
+let counter = 0;
 
 function playRound(player,computer) {
-  let tie = "It is a tie";
-  let computerWin = `You lose! ${computer} beats ${player}`;
-  let playerWin = `You win! ${player} beats ${computer}`;
+  let tieMessage = "It is a tie";
+  let computerWinMessage = `You lose! ${computer} beats ${player}`;
+  let playerWinMessage = `You win! ${player} beats ${computer}`;
+  
   if (player == computer) {
-    return tie;
+    counter;
+    
+    return (tieMessage) + (" | Result: You won: " + counter + " times!");
+   
+    
   } else if ((player == "rock" && computer == "paper") || 
     (player == "paper" && computer == "scissors") || 
     (player == "scissors" && computer == "rock")) {
-    return computerWin;
+    counter;
+    return (computerWinMessage) + (" | Result: You won: " + counter + " times!");
+    
+    
   } else if ((player == "rock" && computer == "scissors") ||
     (player == "paper" && computer == "rock") || 
     (player == "scissors" && computer == "paper")){
-    return playerWin;
+    counter += 1;
+    return (playerWinMessage) + (" | Result: You won " + counter + " times!");   
+  }
+  
+
+}
+
+
+
+// Function game(). To play rock, paper, scissors game with computer five times
+// Returns winner and score of both, the player and the computer
+
+
+function game() {
+  for (let i = 0; i < 5; i++) {
+    let playerSelection = prompt ("Rock, Paper or Scissors?", "your choice").toLowerCase();
+    let computerSelection = computerPlay();
+    console.log("Round: " + (i + 1) + " Your Choice : " + playerSelection + " | " + 
+    "Computer's Choice : " + computerSelection);
+    console.log(playRound(playerSelection,computerSelection));
+  
   }
 }
 
-const playerSelection = "rock";
-const computerSelection = computerPlay();
-console.log("playerSelection : " + playerSelection);
-console.log("computerSelection : " + computerSelection);
-console.log(playRound(playerSelection,computerSelection));
+game();
+
