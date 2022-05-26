496012800a5249cc8e9b7cde12ed47184fc1b0bf
Pablogit config --global user.email psierra.js@gmail.com <pablos@MacBook-Pro-de-Pablo.local>
2022-05-26 12:20:56 +0200

    added computer and user scores

--- javascript.js
+++ javascript.js
@@ -4,8 +4,12 @@ const possibleChoices = document.querySelectorAll('button')
 const resultDisplay = document.getElementById('result');
 const userChoiceDisplay = document.getElementById('user-choice');
 const computerChoiceDisplay = document.getElementById('computer-choice');
+const userScoreDisplay = document.getElementById('userScore');
+const computerScoreDisplay = document.getElementById('computerScore');
 let userChoice;
 let result;
+let userScore = 0;
+let computerScore = 0;
 
 possibleChoices.forEach(possibleChoice => possibleChoice.addEventListener('click', (e) => {
   playerSelection = e.target.id;
@@ -21,10 +25,10 @@ function computerPlay () {
   if (choice === 1) {
     computerChoice = 'Rock';
   }
-  if (choice === 2) {
+  else if (choice === 2) {
     computerChoice = 'Paper';
   }
-  if (choice === 3) {
+  else if (choice === 3) {
     computerChoice = 'Scissors'
   }
   computerChoiceDisplay.innerHTML = computerChoice
@@ -37,17 +41,25 @@ function playRound() {
   
   if (playerSelection === computerChoice) {
     result = 'It is a tie!';
+    userScore = userScore + 0;
+    computerScore = computerScore + 0;
       
   } else if ((playerSelection === 'Rock' && computerChoice === 'Paper') || 
     (playerSelection === 'Paper' && computerChoice === 'Scissors') || 
     (playerSelection === 'Scissors' && computerChoice === 'Rock')) {
     result = 'You Lose!';
+    userScore = userScore + 0;
+    computerScore = computerScore + 1;
     
   } else if ((playerSelection === 'Rock' && computerChoice === 'Scissors') ||
     (playerSelection === 'Paper' && computerChoice === 'Rock') || 
     (playerSelection === 'Scissors' && computerChoice === 'Paper')){
-      
+    result = 'You Win!';
+    userScore += 1;
+    computerScore = computerScore + 0;
   } 
+  userScoreDisplay.innerHTML = userScore;
+  computerScoreDisplay.innerHTML = computerScore;
   resultDisplay.innerHTML = result;
   
 
