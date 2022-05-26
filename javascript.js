7769939cf7bceacfb0174091555f339be9b1df65
Pablogit config --global user.email psierra.js@gmail.com <pablos@MacBook-Pro-de-Pablo.local>
2022-05-26 12:03:11 +0200

    computerPlay: replaced switch with if statements

--- javascript.js
+++ javascript.js
@@ -4,9 +4,9 @@ const possibleChoices = document.querySelectorAll('button')
 const resultDisplay = document.getElementById('result');
 const userChoiceDisplay = document.getElementById('user-choice');
 const computerChoiceDisplay = document.getElementById('computer-choice');
-
 let userChoice;
 let result;
+
 possibleChoices.forEach(possibleChoice => possibleChoice.addEventListener('click', (e) => {
   playerSelection = e.target.id;
   userChoiceDisplay.innerHTML = playerSelection;
@@ -16,17 +16,16 @@ possibleChoices.forEach(possibleChoice => possibleChoice.addEventListener('click
 
 
 function computerPlay () {
-  let randomNumber = Math.floor(Math.random()*(3)+1);
-  switch (randomNumber) {
-    case 1:
-      computerChoice = 'Rock';
-      break;
-    case 2:
-      computerChoice = 'Paper';
-      break;
-    case 3:
-      computerChoice = 'Scissors';
-      break;    
+  let choice = Math.floor(Math.random()*3)+ 1;
+  
+  if (choice === 1) {
+    computerChoice = 'Rock';
+  }
+  if (choice === 2) {
+    computerChoice = 'Paper';
+  }
+  if (choice === 3) {
+    computerChoice = 'Scissors'
   }
   computerChoiceDisplay.innerHTML = computerChoice
 }
