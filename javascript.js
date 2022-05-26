ac088757e6ce6c67a2f846478952e0f4c91c2fe9
Pablogit config --global user.email psierra.js@gmail.com <pablos@MacBook-Pro-de-Pablo.local>
2022-05-26 11:27:55 +0200

    erased commented code

--- javascript.js
+++ javascript.js
@@ -1,5 +1,4 @@
-//function computerPlay. Takes number 1, 2 or 3 randomly and returns
-//rock, papers or scissors depending on the number's value.
+
 
 const possibleChoices = document.querySelectorAll('button')
 const resultDisplay = document.getElementById('result');
@@ -33,11 +32,7 @@ function computerPlay () {
 }
 
 
-//Function playRound(). It takes two parameters (playerSelection and computerSelection)
-//and then returns a string declaring the winner of the round.
 
-// let playerCounter = 0;
-// let computerCounter = 0;
 
 function playRound() {
   
@@ -57,50 +52,4 @@ function playRound() {
   
 
 }
-
-
-
-// Function game(). To play rock, paper, scissors game with computer n times
-// Returns winner and score of both, the player and the computer
-
-// n in game(n) refers to number of times to play the game
-// function game(n) {
-//   for (let i = 0; i < n; i++) {
-//     // let playerSelection = prompt ("Rock, Paper or Scissors?", "your choice").toLowerCase();
-//     let computerSelection = computerPlay();
-//     console.log("Round: " + (i + 1) + " Your Choice : " + playerSelection + " | " + 
-//     "Computer's Choice : " + computerSelection);
-//     console.log(playRound(playerSelection,computerSelection));
-    
-  
-//   }
-
-  //add if block with counter section indicating the winner
-  
-  
-//   if (playerCounter > computerCounter) {
-//     console.log("\nFinal Result: Player wins!");
-//   } else if (playerCounter == computerCounter) {
-//     console.log("\nFinal Result: It is a tie");
-//   } else {
-//     console.log("\nFinal Result: Computer Wins!");
-//   }
-// }
-
-// bottons below
-
-
-
-// Rock.addEventListener("click",function(e) {
-//   playRound("rock", computerPlay());
-//   console.log("Rock has been chosen");
-// });
-// paperBtn.addEventListener("click", function(e) {
-//   playRound("paper", computerPlay());
-//   console.log("You have chosen 'paper'");
-// });
-// scissorsBtn.addEventListener("click", function(e) {
-//   playRound("scissors", computerPlay());
-//   console.log("You have chosen 'scissors'");
-// });
 
